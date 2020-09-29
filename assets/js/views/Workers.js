d79bace004da9468404714c61ea3731821aaff9b
Rafał <rafal@MacBook-Pro-mac.local>
2020-09-29 21:10:42 +0200

    search workers

--- assets/js/views/Workers.js
+++ assets/js/views/Workers.js
@@ -1,20 +1,35 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useState, useRef } from "react"
 
 const Workers = () => {
   const [workerList, setWorkerList] = useState([])
+  const [filteredWorkers, setFilteredWorkers] = useState([])
+
+  const input = useRef()
 
   useEffect(() => {
     fetch('http://localhost:8000/api/workers')
       .then(res => res.json())
-      .then(json => setWorkerList(json))
+      .then(json => {
+        setWorkerList(json)
+        setFilteredWorkers(json)
+      })
   }, [])
+
+  const searchWorkers = (e) => {
+    e.preventDefault()
+    const workers = workerList.filter((worker) => {
+      return worker.name.includes(input.current.value)
+    })
+
+    setFilteredWorkers(workers)
+  }
 
   const addWorker = () => {
     setWorkerList([
       ...workerList,
       {
         id: workerList.length + 1,
-        name: "Leon",
+        name: input.current.value,
       },
     ])
   }
@@ -22,6 +37,16 @@ const Workers = () => {
   return (
     <>
       <h2 className="title is-5">Workers</h2>
+      <div className="field has-addons my-5">
+        <div className="control">
+          <input ref={input} className="input" type="text" placeholder="Find a repository" />
+        </div>
+        <div className="control">
+          <a className="button is-primary" onClick={searchWorkers}>
+            Search
+          </a>
+        </div>
+      </div>
       <table className="table is-bordered">
         <thead>
           <tr>
@@ -30,7 +55,7 @@ const Workers = () => {
           </tr>
         </thead>
         <tbody>
-          {workerList.map((worker) => (
+          {filteredWorkers.map((worker) => (
             <tr key={worker.id}>
               <td>{worker.id}</td>
               <td>{worker.name}</td>
