56349ed4b2245acabe880b22266dcf53f38d87b9
Rafał <rafal@MacBook-Pro-mac.local>
2020-10-12 19:00:07 +0200

    import font awesome

--- assets/js/views/Workers.js
+++ assets/js/views/Workers.js
@@ -2,10 +2,6 @@ import React, { useEffect, useState } from "react"
 import Add from "./Workers/Add"
 import Search from "./Workers/Search"
 import Worker from "./Workers/Worker"
-import Edit from "./Workers/Edit"
-import Delete from "./Workers/Delete"
-import Skills from "./Workers/Skills"
-import AddSkill from "./Workers/AddSkill"
 
 const Workers = () => {
   const [workerList, setWorkerList] = useState([])
@@ -39,33 +35,6 @@ const Workers = () => {
             <tbody>
               {filteredWorkers.map((worker) => (
                 <Worker key={worker.id} data={worker} />
-                // <tr key={worker.id}>
-                //   <td>{worker.id}</td>
-                //   <td>{worker.name}</td>
-                //   <td>{worker.surname}</td>
-                //   <td>
-                //     <Skills skills={worker.skills} worker={worker.id} />
-                //   </td>
-                //   <td>
-                //     <div className="buttons">
-                //       <Delete
-                //         setWorkerList={setWorkerList}
-                //         setFilteredWorkers={setFilteredWorkers}
-                //         id={worker.id}
-                //       />
-                //       <Edit
-                //         setWorkerList={setWorkerList}
-                //         setFilteredWorkers={setFilteredWorkers}
-                //         id={worker.id}
-                //         name={worker.name}
-                //         surname={worker.surname}
-                //       />
-                //       <AddSkill
-                //         worker={worker.id}
-                //       />
-                //     </div>
-                //   </td>
-                // </tr>
               ))}
             </tbody>
           </table>
